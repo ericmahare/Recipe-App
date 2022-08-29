f72dd5462af14cdeaaf938bd6671b1113e8d7e78
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-29 22:58:40 +0100

    Add layout for app pages and some general styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_194241) do
+ActiveRecord::Schema.define(version: 2022_08_29_215326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
