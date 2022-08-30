b8991a030529fd27baf4fabe60431d27242dec27
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-30 19:50:10 +0100

    complete validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :recipes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_many :recipes, counter_cache: true
 
