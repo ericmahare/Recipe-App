4dfa776e9348aff6bd6a1729650e078521d76312
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-30 10:27:09 +0100

    Fix linting errors

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,44 +1,44 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @page = pages(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get pages_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_page_url
     assert_response :success
   end
 
-  test "should create page" do
+  test 'should create page' do
     assert_difference('Page.count') do
-      post pages_url, params: { page: {  } }
+      post pages_url, params: { page: {} }
     end
 
     assert_redirected_to page_url(Page.last)
   end
 
-  test "should show page" do
+  test 'should show page' do
     get page_url(@page)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_page_url(@page)
     assert_response :success
   end
 
-  test "should update page" do
-    patch page_url(@page), params: { page: {  } }
+  test 'should update page' do
+    patch page_url(@page), params: { page: {} }
     assert_redirected_to page_url(@page)
   end
 
-  test "should destroy page" do
+  test 'should destroy page' do
     assert_difference('Page.count', -1) do
       delete page_url(@page)
     end
