b6ca11b4364e39943f42891f021aa73e3eb83c95
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-30 13:29:12 +0100

    Write table migration for the recipe object

--- db/migrate/20220830112825_create_recipes.rb
+++ db/migrate/20220830112825_create_recipes.rb
@@ -1,6 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
+      t.string :name, null: false, default: "", index: true
+      t.time :preparation_time
+      t.time :cooking_time
+      t.text :description
+      t.boolean :public, default: true
+      t.belongs_to :user, index: true, foreign_key: true, counter_cache: true
 
       t.timestamps
     end
