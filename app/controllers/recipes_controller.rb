cbcfdb6e54ce7641c922db46d12ac6cf608653bc
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-31 14:18:10 +0100

    Users not signed in can still  view public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   # GET /recipes or /recipes.json
   def index
