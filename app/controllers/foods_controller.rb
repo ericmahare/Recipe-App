11ab056f00f4f0adde0ef4e1ee0457b7aa71cdc8
ericmahare <erikmahare@gmail.com>
2022-08-31 23:11:38 +0300

    Add new method to the foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,21 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+  def new 
+    @food = Food.new
+  end
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      flash[:success] = 'Food created succesfully'
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
