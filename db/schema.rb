814182ad6da477d36c269af78b3d9431145ba8a3
ericmahare <erikmahare@gmail.com>
2022-08-31 22:25:58 +0300

    Add foods section styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_153716) do
+ActiveRecord::Schema.define(version: 2022_08_31_181435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
