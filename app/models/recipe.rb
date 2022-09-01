b840167293c7ca45b6efcca2c01b34aac44b71ea
Chia Carlyle <chiacarlyle@gmail.com>
2022-09-01 13:20:55 +0100

    Add necessary controller actions for ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
 
   belongs_to :user, counter_cache: true
   has_many :ingredients, dependent: :destroy
-  has_many :foods, through: :ingredients, independent: :destroy
+  has_many :foods, through: :ingredients, dependent: false
 
   def public_recipes
     Recipe.where(public: true).order(created_at: :DESC)
