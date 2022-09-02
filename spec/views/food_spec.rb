b7845227c61fe0b88d502fa88add7d40890943bb
Chia Carlyle <chiacarlyle@gmail.com>
2022-09-02 22:27:54 +0100

    Write tests for the food path

---
+++ spec/views/food_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :view do
+  before :each do
+    @user = User.create(name: 'Eric', email: 'eric@dev.com', password: 'password', password_confirmation: 'password')
+    @food = Food.create(name: 'Chigala', measurement_unit: 'Litter', price: 10, user_id: @user.id)
+  end
+
+  context 'Visit public recipe' do
+    it 'should show public recipe' do
+      visit new_user_session_path
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
+      click_button 'Log in'
+      visit foods_path
+      expect(page).to have_content('Chigala')
+    end
+  end
+end
